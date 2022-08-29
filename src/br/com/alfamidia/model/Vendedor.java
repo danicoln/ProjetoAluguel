38af5b1b97b9f96436e9617de3da3889471c5d43
danicoln <lincolndeveloper@outlook.com>
2022-08-29 20:27:14 -0300

    commit da classe Vendedor

---
+++ src/br/com/alfamidia/model/Vendedor.java
@@ -0,0 +1,11 @@
+package br.com.alfamidia.model;
+
+public class Vendedor extends Pessoa{
+
+	private final double COMISSAO = 0.1;
+	private double salario;
+
+	public Vendedor(String nome, String email, String cidade, String senha) {
+		super(nome, email, cidade, senha);
+	}
+}
