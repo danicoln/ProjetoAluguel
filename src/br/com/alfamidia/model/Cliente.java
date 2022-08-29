529aa4642730be9546e44e772c3c91fc49f78d9a
danicoln <lincolndeveloper@outlook.com>
2022-08-29 20:26:34 -0300

    commit da classe Cliente

---
+++ src/br/com/alfamidia/model/Cliente.java
@@ -0,0 +1,16 @@
+package br.com.alfamidia.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Pessoa{
+	
+	List<Veiculo> veiculos;
+	
+
+	public Cliente(String nome, String email, String cidade, String senha) {
+		super(nome, email, cidade, senha);
+		this.veiculos = new ArrayList<>();
+	}
+
+}
