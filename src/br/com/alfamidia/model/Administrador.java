d1594b7e59a94104de840c96429ccdc319da8f6a
danicoln <lincolndeveloper@outlook.com>
2022-08-29 20:26:02 -0300

    commit da classe Administrador

---
+++ src/br/com/alfamidia/model/Administrador.java
@@ -0,0 +1,11 @@
+package br.com.alfamidia.model;
+
+public class Administrador extends Pessoa {
+
+	private double salario;
+	
+	public Administrador(String nome, String email, String cidade, String senha) {
+		super(nome, email, cidade, senha);
+	}
+
+}
